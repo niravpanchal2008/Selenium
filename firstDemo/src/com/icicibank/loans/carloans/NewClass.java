daf850e61ec062c22d56f42dd4b2c532b6d04fbe
Nirav Panchal <niravpanchal2008@gmail.com>
2018-05-21 07:47:06 +0530

    Added new Class "NewClass1"

---
+++ firstDemo/src/com/icicibank/loans/carloans/NewClass.java
@@ -0,0 +1,12 @@
+package com.icicibank.loans.carloans;
+
+public class NewClass {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("This is NewClass1");
+
+	}
+
+}
