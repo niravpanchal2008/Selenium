0ba5f13908bb1b12718cf2e673ff39fbb1ae4dfa
Nirav Panchal <niravpanchal2008@gmail.com>
2018-05-21 08:02:57 +0530

    Added new message

--- firstDemo/src/com/icicibank/loans/carloans/NewClass.java
+++ firstDemo/src/com/icicibank/loans/carloans/NewClass.java
@@ -6,7 +6,8 @@ public class NewClass {
 	{
 		// TODO Auto-generated method stub
 		System.out.println("This is NewClass1");
-
+		System.out.println("This is NewClass2");
+		System.out.println("This is NewClass3");
 	}
 
 }
