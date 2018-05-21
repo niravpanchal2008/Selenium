1c2ee445c123fdcaf98263ded396c0c3ffc3a0f1
Nirav Panchal <niravpanchal2008@gmail.com>
2018-05-21 08:03:31 +0530

    Revert "Added new message"
    This reverts commit 0ba5f13908bb1b12718cf2e673ff39fbb1ae4dfa.

--- firstDemo/src/com/icicibank/loans/carloans/NewClass.java
+++ firstDemo/src/com/icicibank/loans/carloans/NewClass.java
@@ -6,8 +6,7 @@ public class NewClass {
 	{
 		// TODO Auto-generated method stub
 		System.out.println("This is NewClass1");
-		System.out.println("This is NewClass2");
-		System.out.println("This is NewClass3");
+
 	}
 
 }
